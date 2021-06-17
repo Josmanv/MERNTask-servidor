11fdb3a0163cdca74a5095e37f972e54e8400203
josmanv <josem_lc@hotmail.com>
2021-06-17 17:23:48 +0200

    añadiendo db mongo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("desde index");
+const express = require('express');
+
+//Crear el servidor
+const app = express();
+
+//Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// app.get('/', (req, res) =>{
+//     res.send('Hola Mundo')
+// })
+
+// Arrancar ka app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
