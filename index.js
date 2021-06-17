3c296cb5438ec7b92a3d5c15803c0ce66d1fb83a
josmanv <josem_lc@hotmail.com>
2021-06-17 17:38:07 +0200

    conectando la base de datos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 //Crear el servidor
 const app = express();
+
+// conectar a la base de datos
+conectarDB();
 
 //Puerto de la app
 const PORT = process.env.PORT || 4000;
@@ -13,4 +17,4 @@ const PORT = process.env.PORT || 4000;
 // Arrancar ka app
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
-});
+}); 
