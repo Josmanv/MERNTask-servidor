f97fe1f92feb29135ed114a1c69b931967db5f69
josmanv <josem_lc@hotmail.com>
2021-06-18 20:22:59 +0200

    hash para las contraseñas

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../Models/Usuario');
+const bcrypt = require('bcryptjs');
 
 exports.crearUsuario = async (req, res) => {
 
@@ -15,6 +16,10 @@ exports.crearUsuario = async (req, res) => {
 
         // Crea el nuevo usuario
         usuario = new Usuario(req.body);
+
+        // Hasear el password
+        const salt = await bcrypt.genSaltSync(10);
+        usuario.password = await bcrypt.hashSync(password, salt);
        
         // Guardar Usuario
         await usuario.save();
