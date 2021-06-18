83526b51b4e7dd7563b1ccf3e572910a27e9d225
josmanv <josem_lc@hotmail.com>
2021-06-18 20:45:07 +0200

    validaciones con express validator

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,9 +3,16 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const { check } = require('express-validator');
 
 // Crea un usuario
 // api/usuarios'
-router.post('/', usuarioController.crearUsuario);
+router.post('/',
+[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email debe ser válido').isEmail(),
+    check('password', 'El pass debe tener mínimoo 6 caracteres').isLength({min:6})
+],
+ usuarioController.crearUsuario);
 
 module.exports = router;
