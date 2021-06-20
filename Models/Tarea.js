c9c307bc131ddd1402679fb2223a83b04d127c93
josmanv <josem_lc@hotmail.com>
2021-06-20 22:11:40 +0200

    add modelo Tarea

---
+++ Models/Tarea.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.pluralize(null);
+mongoose.set('useCreateIndex', true);
+
+const TareaSchema = mongoose.Schema({
+    nombre:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    estado:{
+        type: Boolean,
+        default: false
+    },
+    fechaCreacion:{
+        type: Date,
+        default: Date.now()
+    },
+    proyecto:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'proyectos' // esta referecncia debe ser el nombre de la colección, en este caso la llamé proyectos
+    }  
+});
+
+module.exports = mongoose.model('tareas', TareaSchema);
