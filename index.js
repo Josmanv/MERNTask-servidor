7dbdd27cd99421e758b972523cb8302ae56103fe
josmanv <josem_lc@hotmail.com>
2021-06-30 23:30:10 +0200

    cambios cors

--- index.js
+++ index.js
@@ -2,36 +2,28 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-//Crear el servidor
+// crear el servidor
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "domain"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    next();
-  });
-
-
-// conectar a la base de datos
+// Conectar a la base de datos
 conectarDB();
 
-// Habilitar cors
+// habilitar cors
 app.use(cors());
 
-// Habilitsr express.json
-app.use(express.json({extend: true}));
+// Habilitar express.json
+app.use( express.json({ extended: true }));
 
-//Puerto de la app
+// puerto de la app
 const port = process.env.PORT || 4000;
 
 // Importar rutas
-app.use('/api/usuarios' , require('./routes/usuarios'));
+app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-// Arrancar ka app
+// arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
-}); 
+});
