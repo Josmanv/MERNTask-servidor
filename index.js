c5edff40e232062123cbdd739e202ff9eafc9585
josmanv <josem_lc@hotmail.com>
2021-07-01 17:05:51 +0200

    cors

--- index.js
+++ index.js
@@ -2,48 +2,28 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-//Crear el servidor
+// crear el servidor
 const app = express();
 
-  // Configurar cabeceras y cors
-  app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // handle OPTIONS method
-    if ('OPTIONS' == req.method) {
-        return res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
-
-// conectar a la base de datos
+// Conectar a la base de datos
 conectarDB();
 
-//habilitar cors
-app.use(cors({ credentials: true, origin: true }));
-app.options("*", cors());
+// habilitar cors
+app.use(cors());
 
-// Habilitsr express.json
-app.use(express.json({extend: true}));
+// Habilitar express.json
+app.use( express.json({ extended: true }));
 
-//Puerto de la app
+// puerto de la app
 const port = process.env.PORT || 4000;
 
 // Importar rutas
-app.use('/api/usuarios' , require('./routes/usuarios'));
+app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-// Arrancar ka app
+// arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
-}); 
-
-// app.listen((process.env.PORT || 5000), function(){
-//     console.log('listening on *:5000');
-//   });
+});
